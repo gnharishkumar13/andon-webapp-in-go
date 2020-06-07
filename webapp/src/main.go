85aa22d8c4fa1250b8642c862ac00928e0be0c22
gnharishkumar13 <harishkumar_gn@yahoo.com>
2020-06-06 23:20:23 -0400

    template composition

--- webapp/src/main.go
+++ webapp/src/main.go
@@ -8,14 +8,27 @@ import (
 
 func main() {
 
-	content := `{{- /** This is a comment */ -}} this is a template`
-	t := template.New("my first template")
-	t, err := t.Parse(content)
+	childContent := `this is a child template`
+	parentContent := `
+	this is a parent template first line
+	{{ template "child_template" }}
+	this is a parent template second line
+	`
+	parent := template.New("parent_template")
+	child := parent.New("child_template") //Template composition
+
+	_, err := parent.Parse(parentContent)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = t.Execute(os.Stdout, nil)
+
+	_, err = child.Parse(childContent)
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	err = parent.Execute(os.Stdout, nil)
+
 }
