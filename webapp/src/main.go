b63473c0ecf4999297d970aae56e9598ba357bff
gnharishkumar13 <harishkumar_gn@yahoo.com>
2020-06-07 00:03:01 -0400

    pipelines with functions and structs example

--- webapp/src/main.go
+++ webapp/src/main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"os"
 )
 
+type data struct {
+	Message string
+	Answer  int
+}
+
+func (d data) PrintMessage(message string, answer int) string {
+	return fmt.Sprintf("%v : %v", message, answer)
+}
+
 func main() {
 
-	//Pipelines
-	data := 42                            //Pipeline can be struct or function too
-	content := `this is a template {{.}}` //Actions{{}} and . means pipelines that have the entire data
+	//Pipelines                          //Pipeline can be struct or function too
+	content := `{{.PrintMessage .Message .Answer}}` //Actions{{}} and . means pipelines that have the entire data
 	t := template.New("")
 	_, err := t.Parse(content)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = t.Execute(os.Stdout, data)
+	err = t.Execute(os.Stdout, data{
+		Message: "The answer is",
+		Answer:  42,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
