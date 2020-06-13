8a4f52dac6fbdefe5fbb93b71d3a0376be5ba466
gnharishkumar13 <harishkumar_gn@yahoo.com>
2020-06-13 16:30:10 -0400

    Added init scripts and seed data

---
+++ db/init/02_seed.sql
@@ -0,0 +1,16 @@
+INSERT INTO public.workcenters ( wc_name, current_product, wc_status, escalation_level, status_set_at ) VALUES 
+('Assembly Line 1', 'Widgets', 2, 1, NOW() - interval '5 minutes'),
+('Assembly Line 2', 'Widgets', 1, 1, NOW()),
+('Assembly Line 3', 'Widgets', 0, 0, NOW()),
+('Roll Furnace 1', 'Widgets', 0, 0, NOW()),
+('Roll Furnace 2', 'Widgets', 0, 0, NOW()),
+('Roll Furnace 3', 'Widgets', 0, 0, NOW()),
+('Roll Furnace 4', 'Widgets', 0, 0, NOW()),
+('Roll Furnace 5', 'Widgets', 0, 0, NOW()),
+('Transfer Press 1', 'Widgets', 0, 0, NOW()),
+('Transfer Press 2', 'Widgets', 0, 0, NOW()),
+('Transfer Press 3', 'Widgets', 0, 0, NOW()),
+('Progressive Press 1', 'Widgets', 0, 0, NOW()),
+('Progressive Press 2', 'Widgets', 0, 0, NOW()),
+('Progressive Press 3', 'Widgets', 0, 0, NOW()),
+('Progressive Press 4', 'Widgets', 0, 0, NOW());
